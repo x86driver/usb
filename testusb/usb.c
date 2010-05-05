ea63b544aae8f8c642ab755fbc2211b11618235d
Doremi Lin <Doremi.Lin@garmin.com>
2010-05-05 11:48:58 +0800

    把 USB_NODE 改成 /dev/garmin
    為了要在 device 端測試

--- testusb/usb.c
+++ testusb/usb.c
@@ -6,28 +6,36 @@
 #include <unistd.h>
 #include "function.h"
 
-#define USB_NODE "/dev/skel0"
+//#define USB_NODE "/dev/skel0"
+#define USB_NODE "/dev/garmin"
 
 int main(int argc, char **argv)
 {
 	if (argc != 2) {
-		fprintf(stderr, "Usage: %s [read_size]\n", argv[0]);
+		fprintf(stderr, "Usage: %s [read_total_size]\n", argv[0]);
 		exit(1);
 	}
 
 	int fd = open(USB_NODE, O_RDWR);
 	int size = atoi(argv[1]);
-	int ret;
-	char buf[512];
+	int ret = 0;
+	char *buf;
 
 	if (!fd) {
 		perror("open");
 		exit(1);
 	}
+	buf = (char*)malloc(size);
+	if (buf == NULL) {
+		perror("malloc");
+		exit(1);
+	}
+
 	printf("Open %s OK\n", USB_NODE);
 
-	memset(&buf[0], 0, sizeof(buf));
-	ret = read(fd, buf, size);
+	memset((void*)&buf[0], 0, size);
+	ret = read(fd, (void*)&buf[0], size);
+
 	printf("Read %d bytes\n", ret);
 	dump(buf, 0, size);
 
